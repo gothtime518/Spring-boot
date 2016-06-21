aadc09b974caa7365f25a8be5e0e762f5beaf1ca
gothtime518 <gothtime518@gmail.com>
2016-06-21 14:55:31 -0400

    Add files via upload
    add greeting class

---
+++ src/main/java/com/msi/model/Greeting.java
@@ -0,0 +1,14 @@
+package com.msi.model;
+
+/**
+ * Created by wangmengyang on 16/6/21.
+ */
+public class Greeting {
+    private final String hello;
+    public Greeting(String hello) {
+        this.hello = hello;
+    }
+    public String getHello() {
+        return hello;
+    }
+}
